4ee1635edde157c2fec6a8ca4cc94b938ea576b5
Pedro Davi <pedrodavi.cdesa@gmail.com>
2024-10-08 20:09:47 -0300

    Implemented Kafka consumer for emotion data

---
+++ backend/kafkaConsumer.js
@@ -0,0 +1,24 @@
+const { kafka } = require('kafkajs');
+
+const kafka = new Kafka({
+    clientID: 'empathic-credit-system',
+    brokers: ['localhost:9092']
+});
+
+const consumer = kafka.consumer({ groupID: 'emotion-data-group' });
+
+const run = async () => {
+    try {
+        await consumer.connect();
+        await consumer.subscribe({ topic: 'emotion-data', fromBeginning: true });
+
+        await consumer.run({
+            eachMessage: async ({ topic, partition, message }) => {
+                const emotionData = JSON.parse(message.value.toString());
+                console.log('Received emotion data', emotionData);
+            }
+          });
+    } catch (err) {
+      console.error('Error in Kafka consumer:', err);
+    }
+};
