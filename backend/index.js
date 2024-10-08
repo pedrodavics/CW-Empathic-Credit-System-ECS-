64b7ca34181b142e7f1fbea6228a5233f061bb4c
Pedro Davi <pedrodavi.cdesa@gmail.com>
2024-10-08 11:51:56 -0300

    Setup basic Express server

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Empathic Credt System API is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
